3298cff40a7ff0c9675a8840e05f8959aaf489fd
lexusdrumgold <LexusDrumgold@gmail.com>
2018-01-12 10:23:00 -0500

    added comments to js file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
+/* Returns true or false, depending whether or not testEmail (a string),
+is a valid email */
 function isEmail(testEmail) {
   var emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   return emailPattern.test(testEmail.toLowerCase());
 }
 
+/* Checks the email input and determines whether or not to display
+the warning message */
 function validate() {
   var formEmail = $('#userEmail').val();
   if (isEmail(formEmail) === false) {
@@ -16,14 +20,17 @@ function validate() {
   }
 }
 
+/* Checks the email input when the user clicks onto the input */
 $('#userEmail').keyup(function() {
   validate();
 });
 
+/* Checks the email input when the user clicks off the input */
 $('#userEmail').blur(function() {
   validate();
 });
 
+/* Determines whether or not to clear the email input */
 var clicks = 0;
 $('#userEmail').click(function() {
   clicks++;
@@ -32,6 +39,7 @@ $('#userEmail').click(function() {
   }
 });
 
+/* Checks the email input when the user clicks submit */
 $('#submitBtn').click(function(e) {
   e.preventDefault();
   validate();
